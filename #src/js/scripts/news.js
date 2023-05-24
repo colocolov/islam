ed5eca1ad79aa1b1b3f4eedbeff13db047b27ed3
colocolov <colocolov.stas@gmail.com>
2023-05-24 10:40:02 +0300

    upd swiper on mobile

--- #src/js/scripts/news.js
+++ #src/js/scripts/news.js
@@ -1,13 +1,13 @@
 // слайдер на главной
-let numberSlider = 2;
-let spaceSlider = 200;
+// let numberSlider = 2;
+// let spaceSlider = 200;
 
-if (window.innerWidth < 992) {
-    spaceSlider = 100;
-  } if (window.innerWidth < 769) {
-    numberSlider = 1;
-    spaceSlider = 0;
-  }
+// if (window.innerWidth < 992) {
+//     spaceSlider = 100;
+//   } if (window.innerWidth < 769) {
+//     numberSlider = 1;
+//     spaceSlider = 0;
+//   }
 
 const newsSlider = new Swiper(".news__slider", {
   loop: true,
@@ -21,7 +21,22 @@ const newsSlider = new Swiper(".news__slider", {
         el: ".news__count",
         type: "fraction",
   },
-  slidesPerView: numberSlider,
-  spaceBetween: spaceSlider,
+
+  // slidesPerView: numberSlider,
+  // spaceBetween: spaceSlider,
+
+  slidesPerView: 2,
+  spaceBetween: 200,
+
+  breakpoints: {
+    992: {
+      spaceBetween: 100,
+    },
+    769: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+  }
+
 });
 //----- END
